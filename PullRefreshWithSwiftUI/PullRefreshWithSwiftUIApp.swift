8448fe0f5ca9cf6ba2b9c79b1768b7707fb5ffbe
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-26 15:25:29 +0200

    Initial Commit

---
+++ PullRefreshWithSwiftUI/PullRefreshWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullRefreshWithSwiftUIApp.swift
+//  PullRefreshWithSwiftUI
+//
+//  Created by ramil on 26.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct PullRefreshWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
